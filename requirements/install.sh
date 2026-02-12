3eb03dce68608eed55823385bfbf1bad6fdb4a46
ciusca42 <cristianiusca13@gmail.com>
2026-02-12 19:30:22 +0100

    added requirements

---
+++ requirements/install.sh
@@ -0,0 +1,20 @@
+# /bin/bash
+
+apt-get install binutils -y && \
+apt-get install bison -y && \
+apt-get install coreutils && \
+apt-get install diffutils && \
+apt-get install findutils && \
+apt-get install gawk -y && \
+apt install gcc -y && \
+apt install gzip -y && \
+apt-get install m4 -y && \
+apt install make -y && \
+apt install patch -y && \
+apt install perl -y && \
+apt install python-is-python3 && \
+apt install sed && \
+apt install tar && \
+apt install texinfo -y
+
+
